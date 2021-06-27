3cfac536fa5cf4a58763308cce17cbd1a28c67f6
Sandrilja <sandra.horn@runbox.eu>
2021-06-27 23:41:31 +0200

    Update Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -17,6 +17,7 @@ export default function Weather(props) {
   const [weatherData, setWeatherData] = useState({});
   function handleResponse(response) {
     setWeatherData({
+      loaded: true,
       temperature: response.data.main.temp,
       description: response.data.weather[0].description,
       humidity: response.data.main.humidity,
